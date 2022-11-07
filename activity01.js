c23566099694087c4f3889be659ddcfe89cc9c57
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-07 09:35:19 +0530

    test strict inequaliaty operator

--- activity01.js
+++ activity01.js
@@ -148,3 +148,33 @@ else{
     console.log("3 === '3' is false");
 }
 // ---------------------------------------------------------------------------
+
+// strict inequaliaty operator in JavaScript
+if(3 != 3){
+    console.log("3 != 3 is true");
+}
+else{
+    console.log("3 != 3 is false");
+}
+
+if(3 != '3'){
+    console.log("3 != '3' is true");
+}
+else{
+    console.log("3 != '3' is false");
+}
+
+if(3 !== 3){
+    console.log("3 !== 3 is true");
+}
+else{
+    console.log("3 !== 3 is false");
+}
+
+if(3 !== '3'){
+    console.log("3 !== '3' is true");
+}
+else{
+    console.log("3 !== '3' is false");
+}
+// ---------------------------------------------------------------------------
