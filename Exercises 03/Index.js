d589bdb113ad949d7b931e539f148e1111145e1e
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-17 14:38:30 +0530

    test a event listener to a button click

---
+++ Exercises 03/Index.js
@@ -0,0 +1,7 @@
+
+let inputBTN = document.getElementById("input-btn");
+
+inputBTN.addEventListener("click", function() {
+    // this is a function connection using a eventListener in JavaScript
+    console.log("this is the save button eventListener");
+});
