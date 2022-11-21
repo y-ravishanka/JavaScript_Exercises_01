3491b2cbf886b026700f273e3219a2fbb42b496f
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-21 17:43:21 +0530

    push the lead from input field to the myLeads array

--- Exercises 03/Index.js
+++ Exercises 03/Index.js
@@ -1,10 +1,11 @@
 
 const inputBTN = document.getElementById("input-btn");
-const inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el");
 let myLeads = [];
 
 
 inputBTN.addEventListener("click", function() {
     // this is a function connection using a eventListener in JavaScript
-    console.log("this is the save button eventListener");
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
 });
