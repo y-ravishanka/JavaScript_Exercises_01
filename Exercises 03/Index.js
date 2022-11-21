6d6b32dd695110846ec7f39e6094a6b0be4faf8e
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-21 17:33:37 +0530

    change to const varibles and declare myLead array to save leads

--- Exercises 03/Index.js
+++ Exercises 03/Index.js
@@ -1,5 +1,8 @@
 
-let inputBTN = document.getElementById("input-btn");
+const inputBTN = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el")
+let myLeads = [];
+
 
 inputBTN.addEventListener("click", function() {
     // this is a function connection using a eventListener in JavaScript
